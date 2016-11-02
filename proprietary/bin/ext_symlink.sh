fa4119008b4493fe4fa50a7a60f424f029589a4a
lehkeda <computer.boy71@yahoo.com>
2016-11-02 23:28:09 +0200

    Revert "ext_symlink: Remove pty symlink"
    This reverts commit 6a7480f06ae93ef27a1bd4c24dc56b086e7fe034.

--- proprietary/bin/ext_symlink.sh
+++ proprietary/bin/ext_symlink.sh
@@ -2,7 +2,14 @@
 
 term="/dev/pts/* "
 
-if [ "$1" = "-u" ]; then
+if [ "$1" = "-p" ]; then
+	link=`getprop sys.symlink.pty null`
+	if [ "$link" != "null" ]; then
+		rm ${link##${term}}
+		ln -s $link;
+		setprop sys.symlink.notify 0
+	fi
+elif [ "$1" = "-u" ]; then
 	link=`getprop sys.symlink.umts_router`
 	if [ -f ${link##${term}} ]; then
 		rm ${link##${term}}
